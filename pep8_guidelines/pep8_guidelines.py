0ab94f574fae1872e608d41b961a1fc7f8d262de
Victor Timbwa <victortimbwa@gmail.com>
2026-05-27 12:23:34 +0300

    Obtaining and saving output from cisco routers

---
+++ pep8_guidelines/pep8_guidelines.py
@@ -0,0 +1,12 @@
+# PEP8 Guidelines as written by Guido van Rossum, the creator of Python.
+
+# 1. limit all lines to a maximum of 79 characters.
+#2. Linebreak should occur before a binary operator, not after.
+income = (gross_wages
+            + taxable_interest)
+# 3. Imports should be at the top of a file.
+import json
+import requests
+
+# 4. Never use characters I, O, or l as single character variable names.
+# 5. Always use spaces unless you are trying to be consistent with code that already uses tabs..
--- reading_data/cisco.py
+++ reading_data/cisco.py
@@ -1,78 +1,76 @@
-"""
-This module is designed to interact with Cisco appliances
-
-"""
 import netmiko
-import pandas as pd
-import requests
-import pprint
-import json
+import os
+
 
 class CiscoIOS():
     """
-    This class is designed to connect to a cisco IOS appliance and get various data or
+    This class is designed to connect to a cisco IOS appliance and get various data or 
     issue configuration commands
     """
 
-    def __init__(self, ip, port=22, username=None, password=None, device_type=None):
+    def __init__(self, ip, port=22, prompt=False, username=None, 
+                        password=None, device_type='cisco_ios'):
         """
-        The constructor creates an instance variable connected to a cisco IOS router
+        Establishes connection to target device.
+        
+        :param str ip:              ip address of target device.
+        :param int port:            port for connection
+        :param str username:        username for authentication
+        :param str password:        password for authentication
+        :param str device_type:     should not change, class is designed for Cisco IOS
         """
         self.conn = netmiko.ConnectHandler(ip=ip, port=port, username=username,
-                password=password, device_type=device_type)
-        self.hostname = self.conn.send_command('sh run | include hostname').split()[-1]
-
-    def get_interface_names(self):
-        """
-        Returns list containing interface names from show ip interface brief
-        """
-        ip_int_br = self.conn.send_command('sh ip int br', use_textfsm=True)
-        df = pd.DataFrame(ip_int_br)
-        interface_names = df['intf'].to_list()
-        return interface_names
+                        password=password, device_type=device_type)
+        _ = self.conn.send_command('sh run | include hostname')
+        self.hostname = _.split()[-1]
 
     def get_run_cfg(self):
         """
-        Returns running configuration as string from the appliance
+        Returns the running-config from the appliance as a string
         """
-        return self.conn.send_command('sh run')
+        running_config = self.conn.send_command('sh run')
+        return running_config
 
-class CiscoRestconf():
-    """
-    This class is designed to query and manage a Cisco appliance via restconf
-    """
-    def __init__(self, ip, username, password, port=443):
-        self.ip = ip
-        self.username = username
-        self.password = password
-        self.port = str(port)
+    def get_ip_int(self):
+        """
+        Returns the output from show ip interface as a string
+        """
+        ip_int = self.conn.send_command('sh ip int')
+        return ip_int
 
-    def get_interface_info(self, data_type='json'):
+    def get_log(self):
         """
-        Returns dictionary containing JSON interface information
+        Returns the log from the appliance as a string
         """
-        url_base = f'https://' + self.ip + ':' + self.port
-        dn = '/restconf/data/Cisco-IOS-XE-native:native/interface/'
-        uri = url_base + dn
-        headers = {'Accept': 'application/yang-data+' + data_type}
-        request_response = requests.get(uri,headers=headers, auth=(self.username, self.password), verify=False)
-        return request_response.json()
-                                
-    def get_interface_ips(self, interface_info, data_type='json'):
-        """
-        Returns list of interfaces that have a primary IP address
-        """
-        interface_dict = interface_info['Cisco-IOS-XE-native:interface']
-        interface_list = interface_dict['GigabitEthernet']
-        for intf in interface_list:
-            if 'ip' not in intf:
-                interface_list.remove(intf)
-        return json.dumps(interface_list, indent=4)
-
+        log = self.conn.send_command('sh log')
+        return log
+    
 def main():
-   csr = CiscoRestconf('10.254.0.1', username='cisco', password='cisco')
-   with open('csr_interface_IPs.json', 'w') as json_file:
-   print(csr.get_interface_ips(csr.get_interface_info()), file=json_file)
-       
+ #       print(os.getcwd())
+    ip_list = [
+        '10.254.0.1',
+        '10.254.0.2',
+        '10.254.0.3',
+    ]
+    router_list = []
+    for ip in ip_list:
+        conn = CiscoIOS(ip, username='cisco', password='cisco')
+        router_list.append(conn)
+    for router in router_list:
+        base_dir = os.getcwd() + '/routers'
+        router_dir = base_dir + router.hostname + '/'
+        os.mkdir(router_dir, exist_ok=True)
+        data_dict = {
+            'running_config': router.get_run_cfg(),
+            'ip-interface': router.get_ip_int(),
+            'log': router.get_log(),
+        }
+        for output in data_dict:
+            filename = router_dir + output + '.txt'
+            with open (filename, 'w') as f:
+                print(data_dict[output], file=f)
+
 if __name__ == '__main__':
-   main()
+     main()
+     for _ in os.walk(os.walk('routers')):
+         print(_)
